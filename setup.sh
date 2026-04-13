898a4587a0c3d3b23a7d6249cbdbf78950008b0c
Adambhr145 <Adambn145@gmail.com>
2026-04-13 01:21:36 +0100

    Create setup.sh to install npm dependencies and set up the project for development

---
+++ setup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Install npm dependencies
+npm install
+
+# Additional setup commands can be added here
+
+# Notify the user
+echo 'Project setup complete! You can now start development.'
